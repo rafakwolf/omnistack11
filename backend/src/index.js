e6ad1f98bb6df01e5d1749d8ad66d9e7c9330d67
Rafael <rafakwolf@gmail.com>
2020-10-10 19:38:32 -0300

    creates src folder

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
-
-const app = express();
+const routes = require('./routes');
 
 const PORT = 3333;
 
+const app = express();
+app.use(express.json());
+
+app.use(routes);
+
 app.listen(PORT, () => {
     console.info(`App listening on ${PORT}`);
-})
+});
